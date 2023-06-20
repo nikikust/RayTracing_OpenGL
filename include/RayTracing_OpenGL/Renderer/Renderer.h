80658b017e6bd7350bebce5f8458f97e3767756a
Nikikust <lis291@yandex.ru>
2023-06-20 22:24:49 +0300

    Маленькие исправления.

--- include/RayTracing_OpenGL/Renderer/Renderer.h
+++ include/RayTracing_OpenGL/Renderer/Renderer.h
@@ -8,41 +8,37 @@
 class Renderer
 {
 public:
-	Renderer(DataStorage& data_storage);
-	Renderer() = delete;
-	Renderer(Renderer&) = delete;
-	Renderer(Renderer&&) = delete;
+    Renderer(DataStorage& data_storage);
+    Renderer() = delete;
+    Renderer(Renderer&) = delete;
+    Renderer(Renderer&&) = delete;
 
-	~Renderer();
+    ~Renderer();
 
-	// --- //
+    // --- //
 
-	void update();
-	void draw();
+    void update();
+    void draw();
 
-	void on_resize(bool init = false);
+    void on_resize(bool init = false);
 
 private:
-	//void prepare_shader();
+    void prepare_buffers();
+    void prepare_shaders();
 
-	//void use_shader();
-
-	void prepare_buffers();
-	void prepare_shaders();
-
-	gears::Color trace_ray(const tracer::Ray& ray);
+    gears::Color trace_ray(const tracer::Ray& ray);
 
 
-	DataStorage& data_storage_;
-	
-	GLuint shader_program;         //
-	GLuint id_VAO;                 // for shader
-	GLuint id_VBO;                 //
-	GLuint id_UBO;                 //
-	GLfloat* dotPositions;         // 
-	GLfloat* dotColors;            //
-	GLintptr size_of_dotPositions; // for VBO
-	GLintptr size_of_dotColors;    // 
+    DataStorage& data_storage_;
+    
+    GLuint shader_program;         //
+    GLuint id_VAO;                 // for shader
+    GLuint id_VBO;                 //
+    GLuint id_UBO;                 //
+    GLfloat* dotPositions;         // 
+    GLfloat* dotColors;            //
+    GLintptr size_of_dotPositions; // for VBO
+    GLintptr size_of_dotColors;    // 
 
-	std::vector<int> m_ImageHorizontalIter, m_ImageVerticalIter;
+    std::vector<int> m_ImageHorizontalIter, m_ImageVerticalIter;
 };
